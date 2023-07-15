6800a35f88cc78cc2350c3727dc8853ad99c7381
mostafa amiri <mostafa.amiri.62@gmail.com>
2023-07-15 16:57:33 +0330

    Update eveluation.py

--- deeplearning/eveluation.py
+++ deeplearning/eveluation.py
@@ -1,12 +1,13 @@
 from tqdm import tqdm
 
 def evaluate(model, dataloader, loss_fn):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.eval()
     total_loss = 0
     iter = 0
     for X, y in tqdm(dataloader):
-        X = X.cuda()
-        y = y.cuda()
+        X = X.to(device)
+        y = y.to(device)
         pred = model(X)
         loss = loss_fn(pred, y)
         total_loss = (total_loss * iter + loss.detach().item())/(iter+1)
