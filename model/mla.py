ca8d3ee3bbcc382998db877bea6f0505d96cec0c
mostafa amiri <mostafa.amiri.62@gmail.com>
2023-07-17 07:40:45 +0330

    Update mla.py

--- model/mla.py
+++ model/mla.py
@@ -8,6 +8,9 @@ class MLA(nn.Module):
     self.conv2 = nn.Conv2d(input_channel2, 1, 1, bias=True)
     self.input_channel2 = input_channel2
   def forward(self, X1, X2):
-    alpha = torch.reshape(torch.nn.functional.softmax(torch.flatten(torch.nn.functional.tanh(self.conv2(X2)), start_dim=1),dim=1),(X2.shape[0], 1, X2.shape[-2], X2.shape[-1]))
-    d_X1  =torch.nn.functional.interpolate(self.conv1(X1), (X2.shape[-2], X2.shape[-1]), align_corners=False, antialias=True, mode='bilinear')
+    alpha = torch.reshape(
+      torch.nn.functional.softmax(torch.flatten(torch.nn.functional.tanh(self.conv2(X2)), start_dim=1),dim=1),
+      (X2.shape[0], 1, X2.shape[-2], X2.shape[-1]))
+    d_X1  =torch.nn.functional.interpolate(self.conv1(X1), (X2.shape[-2], X2.shape[-1]),
+                                           align_corners=False, antialias=True, mode='bilinear')
     return X2 + torch.mul(d_X1, alpha).repeat(1,self.input_channel2,1,1)
